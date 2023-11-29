ef0cf0798e1d6ea30c6ae4790ec1afcd4a95c376
MelRaust <147696053+MelRaust@users.noreply.github.com>
2023-11-29 12:13:21 +0100

    maj

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <?php wp_head();?>
 </head>
 <body <?php body_class();?>>
+<!--
 <header>
         <div class="container-header">
             <div class="row d-flex">
@@ -29,3 +30,20 @@
             </div>
         </div>
 </header>
+                                -->
+    <header>
+        <div class="container-header">
+            <div class="row d-flex">
+                <div class="col-12">
+                    <div class="move-header-lien">
+                        <ul class="header-lien d-flex">
+                            <li class="li-acceuil"><a class="lien-acceuil" href="http://localhost/wordpress/">Accueil</a></li>
+                            <li class="li-apropos-contact"><a class="lien-apropos-contact" href="http://localhost/wordpress/a-propos/">A propos</a></li>
+                            <li class="li-apropos-contact"><a class="lien-apropos-contact" href="http://localhost/wordpress/contact/">Contact</a></li>
+                        </ul>  
+                    </div>                   
+                                                            
+                </div>
+            </div>
+        </div>
+    </header>
